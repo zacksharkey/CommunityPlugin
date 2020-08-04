1c0b9ebfd32c58aee06f737e4d39de6714670ef6
Zachary Sharkey <yekrahs@gmail.com>
2020-08-04 08:54:56 -0400

    STashing changes

---
+++ CommunityPlugin/Non Native Modifications/PrintFormToEfolder.cs
@@ -0,0 +1,45 @@
+﻿using CommunityPlugin.Objects;
+using CommunityPlugin.Objects.Interface;
+using EllieMae.Encompass.Automation;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CommunityPlugin.Non_Native_Modifications
+{
+    public class PrintFormToEfolder : Plugin, ILoanOpened
+    {
+        public override bool Authorized()
+        {
+            return PluginAccess.CheckAccess(nameof(PrintFormToEfolder));
+        }
+
+        public override void LoanOpened(object sender, EventArgs e)
+        {
+            //Add Button
+            Button b = new Button();
+            FormWrapper.EncompassForm.Controls.Add(b);
+            b.Click += B_Click;
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            (EncompassApplication.Screens[EncompassScreen.Loans] as LoansScreen).Print(((EncompassApplication.Screens[EncompassScreen.Loans] as LoansScreen).CurrentForm.Name));
+           
+        }
+
+        private void Print(EllieMae.Encompass.Forms.Form f)
+        {
+            //EncompassApplication.Session.ServerEvents.
+            //Rectangle bounds = this.Bounds;
+            //using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            //{
+            //    using (Graphics g = Graphics.FromImage(bitmap))
+            //    {
+            //        g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+            //    }
+            //    bitmap.Save("C://test.jpg", ImageFormat.Jpeg);
+            //}
+        }
+    }
+}
