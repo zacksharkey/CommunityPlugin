2586e1c96a84fe7f1d7dd38306577a4b3d69db49
Zachary Sharkey <yekrahs@gmail.com>
2021-02-12 01:54:11 -0500

    Update Config Form

--- CommunityPlugin/Config/ExportServicePlugin_Config.Designer.cs
+++ CommunityPlugin/Config/ExportServicePlugin_Config.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFieldID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(292, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Export Field ID";
+            // 
+            // txtFieldID
+            // 
+            this.txtFieldID.Location = new System.Drawing.Point(94, 6);
+            this.txtFieldID.Name = "txtFieldID";
+            this.txtFieldID.Size = new System.Drawing.Size(192, 20);
+            this.txtFieldID.TabIndex = 2;
+            // 
+            // ExportServicePlugin_Config
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(371, 35);
+            this.Controls.Add(this.txtFieldID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.MaximumSize = new System.Drawing.Size(387, 74);
+            this.MinimumSize = new System.Drawing.Size(387, 74);
+            this.Name = "ExportServicePlugin_Config";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ExportServicePlugin_Config";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFieldID;
     }
 }
