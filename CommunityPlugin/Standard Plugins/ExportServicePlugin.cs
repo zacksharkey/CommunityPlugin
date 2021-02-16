2a9bd12a11e08f4618292c060cd7c90d37a22741
Zachary Sharkey <yekrahs@gmail.com>
2021-02-16 09:48:52 -0500

    Added support for more than one service button on a page.

--- CommunityPlugin/Standard Plugins/ExportServicePlugin.cs
+++ CommunityPlugin/Standard Plugins/ExportServicePlugin.cs
@@ -9,6 +9,7 @@ using EllieMae.EMLite.RemotingServices;
 using EllieMae.Encompass.Automation;
 using EllieMae.Encompass.BusinessObjects.Loans;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CommunityPlugin.Standard_Plugins
@@ -16,7 +17,7 @@ namespace CommunityPlugin.Standard_Plugins
     public class ExportServicePlugin:Plugin, ILoanOpened, IFormLoaded
     {
         private ExportServiceConfigs Config;
-        private ExportServiceConfig CurrentConfig;
+        private List<ExportServiceConfig> CurrentConfigs;
         public override void Configure()
         {
             ExportServicePlugin_Config form = new ExportServicePlugin_Config();
@@ -31,12 +32,12 @@ namespace CommunityPlugin.Standard_Plugins
         public override void FormLoaded(object sender, FormChangeEventArgs e)
         {
             string form = e.Form.Name;
-            CurrentConfig = Config.Configs.FirstOrDefault(x => x.Forms.Contains(form));
-            if(CurrentConfig != null)
+            CurrentConfigs = Config.Configs.Where(x => x.Forms.Contains(form)).ToList();
+            foreach (ExportServiceConfig config in CurrentConfigs)
             {
-                foreach(EllieMae.Encompass.Forms.Control c in e.Form.FindControlsByType(typeof(EllieMae.Encompass.Forms.Button)))
+                foreach (EllieMae.Encompass.Forms.Control c in e.Form.FindControlsByType(typeof(EllieMae.Encompass.Forms.Button)))
                 {
-                    if(c.ControlID.Equals(CurrentConfig.ExportFieldID))
+                    if (c.ControlID.Equals(config.ExportControlID))
                     {
                         EllieMae.Encompass.Forms.Button b = (EllieMae.Encompass.Forms.Button)c;
                         b.Click += B_Click;
@@ -47,9 +48,13 @@ namespace CommunityPlugin.Standard_Plugins
 
         private void B_Click(object sender, EventArgs e)
         {
-            ServiceSetting setting = ServicesMapping.GetServiceSettingFromID(CurrentConfig.Service);
+            string btnID = ((EllieMae.Encompass.Forms.Button)sender).ControlID;
+            ExportServiceConfig config = CurrentConfigs.FirstOrDefault(x => x.ExportControlID.Equals(btnID));
+            if (config == null || string.IsNullOrEmpty(config.Service))
+                return;
+
+            ServiceSetting setting = ServicesMapping.GetServiceSettingFromID(config.Service);
             new ExportService(Session.LoanDataMgr, setting).ProcessLoans(new string[] { EncompassApplication.CurrentLoan.Guid });
         }
-
     }
 }
