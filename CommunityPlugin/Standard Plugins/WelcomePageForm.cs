ab531f93927b6bd966d988546abe3b7740398e67
Zachary Sharkey <yekrahs@gmail.com>
2021-06-29 10:51:36 -0400

    Added request to get rid of login welcome page.

---
+++ CommunityPlugin/Standard Plugins/WelcomePageForm.cs
@@ -0,0 +1,17 @@
+﻿using CommunityPlugin.Objects;
+using CommunityPlugin.Objects.Args;
+using CommunityPlugin.Objects.Interface;
+
+namespace CommunityPlugin.Standard_Plugins
+{
+    public class WelcomePageForm : Plugin, INativeFormLoaded
+    {
+        public override void NativeFormLoaded(object sender, FormOpenedArgs e)
+        {
+            if (e.OpenForm.Name.Equals(nameof(WelcomePageForm)))
+            {
+                e.OpenForm.Close();
+            }
+        }
+    }
+}
